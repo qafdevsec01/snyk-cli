6fd0ef6c0f4dd5b89136f5f4ce144d32b1f985cf
Catalina Oyaneder <cat2608@gmail.com>
2023-09-25 10:27:20 +0100

    chore: add formatting to Python code

--- scripts/download_go.py
+++ scripts/download_go.py
@@ -8,6 +8,7 @@ import zipfile
 import tempfile
 import sys
 
+
 # determine go binary to download and extract
 def get_go_binary_name(go_os, go_arch, go_version):
     filename = "go" + go_version + "." + go_os + "-" + go_arch
@@ -21,10 +22,11 @@ def get_go_binary_name(go_os, go_arch, go_version):
     print("Binary to download: " + filename)
     return filename
 
+
 # calculate sha256sum of file
 def calculate_sha256sum(file):
     print("Calculating sha256sum of file: " + file)
-    
+
     try:
         with open(file, "rb") as f:
             sha256sum = hashlib.sha256(f.read()).hexdigest()
@@ -34,15 +36,19 @@ def calculate_sha256sum(file):
 
     return sha256sum
 
+
 # extract sha256sum from file contents
 def extract_sha256sum_from_file(file):
     print("Extracting sha256sum from file: " + file)
 
     with open(file, "r") as f:
-        first_line = f.readline().strip()  # Read the first line and remove leading/trailing whitespace
+        first_line = (
+            f.readline().strip()
+        )  # Read the first line and remove leading/trailing whitespace
         first_word = first_line.split()[0]  # Sp
-    
+
     return first_word
+
 
 # download a file
 def download_file(download_path, filename, base_url):
@@ -50,15 +56,16 @@ def download_file(download_path, filename, base_url):
     file_path = os.path.join(download_path, filename)
 
     print("Downloading from: " + url + " to: " + file_path)
-    
+
     try:
         urllib.request.urlretrieve(url, file_path)
         print("Download complete")
     except urllib.error.URLError as e:
         print("Error while downloading the file:", e)
         sys.exit(1)
-    
+
     return file_path
+
 
 # extract tar file
 def extract_tar(tar_gz_file, extract_path):
@@ -71,47 +78,57 @@ def extract_tar(tar_gz_file, extract_path):
         print("Error while extracting the tar file: ", e)
         sys.exit(1)
 
+
 # unzip zip file
 def unzip_file(zip_file, extract_path):
     print("Unzipping: " + zip_file + " to: " + extract_path)
     try:
-        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        with zipfile.ZipFile(zip_file, "r") as zip_ref:
             zip_ref.extractall(extract_path)
             print("Unzipping complete")
     except zipfile.BadZipFile as e:
         print("Error while unzipping the file: ", e)
         sys.exit(1)
 
+
 # setup argparse
 def init_argparse():
     parser = argparse.ArgumentParser(
-        prog="download_go",
-        description="Download and install a specific version of Go."
-        )
+        prog="download_go", description="Download and install a specific version of Go."
+    )
     parser.add_argument("version", help="Version of Go to download (e.g., 1.20)")
-    parser.add_argument("--go_os",
+    parser.add_argument(
+        "--go_os",
         help="OS to download for (e.g., linux, windows, darwin)",
         choices=["linux", "windows", "darwin"],
-        required=True)
-    parser.add_argument("--go_arch",
+        required=True,
+    )
+    parser.add_argument(
+        "--go_arch",
         help="Architecture to download for (e.g., amd64, arm64)",
         choices=["amd64", "arm64", "armv6l"],
-        required=True)
-    parser.add_argument("--base_url",
+        required=True,
+    )
+    parser.add_argument(
+        "--base_url",
         help="Base URL to download from (e.g., https://storage.googleapis.com/golang/)",
-        default="https://storage.googleapis.com/golang/")
-    parser.add_argument("--extraction_path",
+        default="https://storage.googleapis.com/golang/",
+    )
+    parser.add_argument(
+        "--extraction_path",
         help="Path to download the file to (e.g., /tmp)",
-        default=os.getcwd())
+        default=os.getcwd(),
+    )
 
-    return  parser.parse_args()
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = init_argparse()
 
     # check if go is cached, before trying to download and extract it
     file_dir = os.path.join(os.path.abspath(args.extraction_path), "go")
-    if  os.path.exists(file_dir):
+    if os.path.exists(file_dir):
         print("Restored from cache, skipping download and extraction")
         sys.exit(0)
 
@@ -120,14 +137,16 @@ if __name__ == "__main__":
 
     # determine the go binary to download
     binary_name = get_go_binary_name(args.go_os, args.go_arch, args.version)
-    
+
     # download the go binary
     binary_file = download_file(download_path, binary_name, args.base_url)
     # get the sha256sum of the downloaded file
     binary_file_sha256 = calculate_sha256sum(binary_file)
 
     # download the sha256sum file for the go binary
-    sha256sum_file = download_file(download_path, binary_name + ".sha256", args.base_url)
+    sha256sum_file = download_file(
+        download_path, binary_name + ".sha256", args.base_url
+    )
 
     # get the expected sha256sum for the downloaded file
     expected_sha256sum = extract_sha256sum_from_file(sha256sum_file)
